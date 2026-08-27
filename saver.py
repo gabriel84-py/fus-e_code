02bf930b0cd2d5be789b17ebe8dbb83100f91ada
gabriel84-py <hexahack.g8430@gmail.com>
2026-08-27 18:00:01 +0200

    boot

--- saver.py
+++ saver.py
@@ -1,11 +1,11 @@
 import shutil
 rxtx = input('choisit si c est le RX ou le TX : ')
 if rxtx == 'rx':
-    SOURCE = "/Users/gabrieljeanvermeille/PycharmProjects/couillonne-de-la-lune/code-recepteur.py"
+    SOURCE = "/Users/gabrieljeanvermeille/PycharmProjects/couillonne-de-la-lune/src/code-recepteur.py"
 if rxtx == 'tx':
-    SOURCE = "/Users/gabrieljeanvermeille/PycharmProjects/couillonne-de-la-lune/code-transiver.py"
+    SOURCE = "/Users/gabrieljeanvermeille/PycharmProjects/couillonne-de-la-lune/src/code-transiver.py"
 if rxtx == 'bench':
-    SOURCE = "/Users/gabrieljeanvermeille/PycharmProjects/couillonne-de-la-lune/benchbaro.py"
+    SOURCE = "/Users/gabrieljeanvermeille/PycharmProjects/couillonne-de-la-lune/tests/benchbaro.py"
 DEST = "/Volumes/CIRCUITPY/code.py" # adapte le chemin selon ton OS
 shutil.copy(SOURCE, DEST)
 print("Copié vers la carte.")
